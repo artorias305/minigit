ec02cad05e081133f7ebf457900f101b9d51f479
artorias305 <105161738+artorias305@users.noreply.github.com>
2026-05-31 17:34:12 -0300

    Validate object hash length in CatFile
    Return an error if the provided hash is not 40 characters
    and add fmt import for error formatting

--- commands/cat-file.go
+++ commands/cat-file.go
@@ -1,8 +1,14 @@
 package commands
 
-import "os"
+import (
+	"fmt"
+	"os"
+)
 
 func CatFile(hash string) (string, error) {
+	if len(hash) != 40 {
+		return "", fmt.Errorf("invalid hash length: %d", len(hash))
+	}
 	prefix := hash[:2]
 	suffix := hash[2:]
 	path := ".minigit/objects/" + prefix + "/" + suffix
